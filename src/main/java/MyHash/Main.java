07d64c63570987036887c14f5c5ad4dcc695aa24
guruiqin <guruiqin@zhihu.com>
2023-08-17 08:42:42 +0800

    add

--- src/main/java/MyHash/Main.java
+++ src/main/java/MyHash/Main.java
@@ -95,6 +95,52 @@ public class Main {
         return res;
     }
 
+
+    //15 三数相加
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        if (nums[0] > 0){
+            return res;
+        }
+
+        for (int i = 0;i<nums.length;i++){
+            if (i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length-1;
+
+            while (left < right){
+                int sum = nums[i]  + nums[left] + nums[right];
+
+                if (sum > 0){
+                    right--;
+                }else if (sum < 0){
+                    left++;
+                }else {
+
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+
+                    while (left < right && nums[right] == nums[right-1]){
+                        right--;
+                    }
+
+                    while (left < right && nums[left] == nums[left+1]){
+                        left--;
+                    }
+
+                    right--;
+                    left++;
+                }
+            }
+        }
+
+        return res  ;
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
     }
